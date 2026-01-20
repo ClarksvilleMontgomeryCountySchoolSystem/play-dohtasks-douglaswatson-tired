3c5e51c721c7e6ac553a435911dee5772a58e790
douglaswatson-tired <douglas.watson777@students.cmcss.net>
2026-01-20 11:31:47 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,31 +1,30 @@
-
 def main():
-    # Enter your color.
-    color1 = "black"
-
-    print(f"1) Use {color1} to roll a medium-sized ball.\n")
-    # Collect input. Use choice1 Prompt:"big or small? "
-    choice1=input("big or small? ")
-    # == Is used to test for equality.
-    if choice1 == "big":
-        print("2) Flatten the ball into a wide disc.\n")
-    else:
-        print("2) Keep it as a ball.\n")
-    print("3) Roll four small pieces into short ropes.\n")
-    print("4) Attach them to the bottom.\n")
-    print(f"5) Use {color1} to roll a small ball.\n")
-    print("6) Attach it to one side.\n")
-    #Go to user - Include a space after the ? in your prompt.
-    # Collect input. Use choice2 Prompt:"option 1 or option 2? "
-    choice2=input("option 1 or option 2? ")
+    color1 = ""
+    color2 = "brown"
+    print(f"1) Use {color2} to roll a ball.")
+    # Collect input. Use choice1 Prompt:"1, 2, or 3? "
+    choice1=input("1, 2, or 3? ")
+    if choice1 == "1":
+        print("2) Make the ball flat.\n")
+    elif choice1 == "2": # elif is a second check only if the first check fails.
+        print("2) Form the ball into an egg shape.\n")
+    else: # else runs if all previous checks fail.
+        print("2) Keep it round.")
+    print(f"3) Use {color2} to roll two thin ropes.")
+    # Collect input. Use choice2 Prompt:"A or B? "
+    choice2=input("A or B? ")
     # Use == to check the User's choice.
+    if choice2 == "A":
+    # Use the correct string.
     # Remember you are checking equality to a string. You must use quotes.
-    if choice2 == "option 1":
-        print("7) Pinch the small ball to make it pointy.\n")
+    # TODO Write a conditional to compare choice2 & A
+        print("4) Pinch off pieces of the thin ropes to make and attach spots.\n")
+    # TODO What should go here?
     else:
-        print("7) Keep the small ball round.\n")
-    print("8) Poke two tiny holes in the small ball for eyes.\n")
-    print('9) Write "Turtle" on the name card.')
+        print("4) Use the ropes to make stripes.\n")
+    print("5) Add two tiny dots for eyes on the front.")
+    choice3=input()
+    print(f") Write {choice3} on the name card.")
 
 if __name__ == "__main__":
     main()
